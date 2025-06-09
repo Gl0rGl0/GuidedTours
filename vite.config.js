999f459dd9a9dde3d1d77f36316499db3849ee4d
giorgio.felappi@intersail.it <gaionifelappi@gmail.com>
2025-06-09 11:17:17 +0200

    decomment

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,6 @@ export default defineConfig({
             ],
             refresh: true,
         }),
-        // Remove Tailwind plugin: tailwindcss(),
     ],
     // Add resolve alias for bootstrap
     resolve: {
