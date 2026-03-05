d4343fa92407f2c9e2b8513ae6adab0a92a6e3d0
daniel-mac <barbetti.daniel3@gmail.com>
2026-03-05 10:28:07 +0100

    fix: {locale} always set in routing

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Gate; 
 use Illuminate\Support\Facades\Event;
+use Illuminate\Support\Facades\URL;
 use App\Models\User;
 use Illuminate\Contracts\Debug\ExceptionHandler;
 use App\Exceptions\Handler;
@@ -31,6 +32,18 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        // parameter 'locale' set as the current one if present, or the default one from config/app.php
+        // this version should be safe even if {locale} is not in the route, but in general it is better to put it in
+        // non-safe version below
+        URL::defaults([
+            'locale' => in_array(request()->segment(1), ['it', 'en'])
+                        ? request()->segment(1) 
+                        : config('app.locale')
+        ]);
+
+        // non-safe version:
+        // URL::defaults(['locale' => request()->segment(1) ?: config('app.locale')]);
+
         // Define Gate roles
         Gate::define('configurator', function (User $user) {
             return $user->hasRole('Admin');
